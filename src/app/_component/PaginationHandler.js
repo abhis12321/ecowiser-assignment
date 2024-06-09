732d850ef9dbe627324054f249f8213b15324c82
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-09 14:51:05 +0530

    Some styling added

--- src/app/_component/PaginationHandler.js
+++ src/app/_component/PaginationHandler.js
@@ -9,9 +9,9 @@ export default function PaginationHandler({ totalNotes , paginate }) {
 
     return (
         <footer>
-            <div className="pagination">
+            <div className="pagination w-[98%] max-w-[100px] mx-auto flex gap-2 overflow-auto p-1">
                 {pageNumbers.map((number) => (
-                    <div key={number} className="page-item">
+                    <div key={number} className="ring-1 rounded py-2 px-4 w-fit font-semibold hover:bg-green-600/40 hover:ring-violet-600 hover:text-violet-900">
                         <p onClick={() => paginate(number)} className="">
                             {number}
                         </p>
--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,14 @@ import PaginationHandler from './_component/PaginationHandler';
 
 export default function page() {
   const [currentNotes, setCurrentNotes] = useState([]);
-  const [notes , setNotes] = useState();
+  const [notes , setNotes] = useState([{} , {}]);
 
   const paginate = (start) => {
-    currentNotes = [];
+    let currentNotePages = [];
     for(let i = start-1; i < start + 5 && i < notes.length; i++) {
-      currentNotes.push(notes[i]);
+      currentNotePages.push(notes[i]);
     }
-    setCurrentNotes([...currentNotes]);
+    setCurrentNotes([...currentNotePages]);
   }
 
   return (
