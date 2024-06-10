ccce84958125b77a05f103a7817b15309a4672c0
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-10 18:10:40 +0530

    API is use in home page for cloud storage for note

--- src/app/_component/NoteBox.js
+++ src/app/_component/NoteBox.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 import NoteForm from './NoteForm';
 
 export default function NoteBox({ note, saveNote, exist }) {
+  console.log(note);
   const [editable, setEditable] = useState(false);
   return (
     <div className='ring-1 rounded min-w-[300px] flex gap-2 flex-col items-center justify-center bg-white' onClick={e => setEditable(true)}>
       <div className="relative p-4 flex gap-2 flex-col items-center justify-center">
-        <p className={`absolute top-2 right-2 p-1 hover:scale-125 cursor-pointer`}>📌</p>
-        <h3 className="text-lg font-bold text-red-950">{note.title}</h3>
-        <p className="text-xs text-blue-900/40 font-bold">{note.tagline}</p>
-        <p className="text-sm text-green-950/60 ">{note.body}</p>
+        <p className={`absolute top-2 right-[-2.5rem] p-1 hover:scale-125 cursor-pointer`}>📌</p>
+        <h3 className="text-lg font-bold text-red-950">{note?.title}</h3>
+        <p className="text-xs text-blue-900/40 font-bold">{note?.tagline}</p>
+        <p className="text-sm text-green-950/60 ">{note?.body}</p>
       </div>
       {
         editable && <NoteForm note={note} setEditable={setEditable} saveNote={saveNote} exist={exist} />
--- src/app/page.js
+++ src/app/page.js
@@ -3,38 +3,24 @@ import React, { useEffect, useState } from 'react'
 import NoteForm from './_component/NoteForm'
 import PaginationHandler from './_component/PaginationHandler';
 import NotesContainer from './_component/NodesContainer';
+import axios from 'axios';
 
 export default function page() {
   const [newNote , setNewNote] = useState(false);
   const [currentNotes, setCurrentNotes] = useState([]);
-  const [notes, setNotes] = useState([
-    { title: "AKTU", tagline: "1hahaha", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "2HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "3HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "4HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "5HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "6HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "7HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "8HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "9HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "10HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "11HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "12HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "13HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "14HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "15HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "16HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "17HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "18HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "19HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "20HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "21HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-    { title: "HBTU", tagline: "22HAHAHAHA", body: "hello dear it's the body of the note #nodebody" },
-  ]);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api')
+        .then(res => res.data)
+        .then(data => data.notes) 
+        .then(Notes => setNotes([...Notes]))
+        .catch(error => console.log(error.message));
+  }, []);
 
   useEffect(() => {
     paginate(0);
-  }, []);
+  } , [notes]);
 
   const paginate = (start) => {
     let currentNotePages = [];
@@ -47,8 +33,10 @@ export default function page() {
   const saveNote = async (note , exist) => {
     if(!exist && exist !== 0) {
       notes.push(note);
+      let newNote = await axios.post('/api' , note);
     } else {
-      notes[exist] = note;
+      let newNote = await axios.put('/api' , note);
+      notes[exist] = newNote;
     }
 
     // console.log(exist , note);
@@ -58,6 +46,7 @@ export default function page() {
 
   return (
     <div className='h-[100vh] flex flex-col items-center justify-evenly w-full'>
+      <button onClick={e => console.log(notes.length)}>LOG</button>
       {
         newNote ? 
         <NoteForm saveNote={saveNote}  setEditable = {setNewNote} />
